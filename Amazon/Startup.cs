e3ef6ad728317adf04b36ce176dc3f6d67fd2814
Kaden <kaden.souter@gmail.com>
2022-03-16 16:45:55 -0600

    For bootstrap I added place holders for the username and password in the Login.cshtml folder and changed the logout button.

--- Amazon/Startup.cs
+++ Amazon/Startup.cs
@@ -2,6 +2,7 @@ using Amazon.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,6 +32,12 @@ namespace Amazon
                options.UseSqlite(Configuration["ConnectionStrings:BookDBConnection"]);
            });
 
+            services.AddDbContext<AppIdentityDBContext>(options => 
+            options.UseSqlite(Configuration["ConnectionStrings:IdentityConnection"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDBContext>();
+
             services.AddScoped<IBookRepository, EFBookRepository>();
             services.AddScoped<IPurchaseRepository, EFPurchaseRepository>();
 
@@ -57,6 +64,9 @@ namespace Amazon
             app.UseSession();
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
 
@@ -79,6 +89,8 @@ namespace Amazon
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
             });
+
+            IdentitySeedData.EnsurePopulated(app);
         }
     }
 }
